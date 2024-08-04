c819fab035daee73c22393b1d8aa6244c2858d86
swan14gg <swan.101114@gmail.com>
2024-08-04 22:24:04 +0900

    add training

---
+++ src/chapter01/training_1.11.ts
@@ -0,0 +1,13 @@
+{
+  function increment(x: number): number {
+    return x + 1;
+  }
+
+  function getFirstCharacter(s: string): string {
+    return s?.[0] ?? "";
+  }
+
+  function wordScore(word: string): number {
+    return word.length;
+  }
+}
